27bd3b52cdfbd77fa3881f1c863d2ac20205214d
Kostiantyn Plotnyk <kostiantyn.plotnyk@gmail.com>
2025-09-07 07:22:59 +0200

    hw2 made

--- test.js
+++ test.js
@@ -1,28 +1,28 @@
-const age = 11;
-console.log(age);
+// const age = 11;
+// console.log(age);
 
-const name = "Lev";
-console.log(name);
+// const name = "Lev";
+// console.log(name);
 
-const isStudent = true;
-console.log(isStudent);
+// const isStudent = true;
+// console.log(isStudent);
 
-const myString = "чиназиз";
-console.log(myString);
+// const myString = "чиназиз";
+// console.log(myString);
 
-let myNumber = 8;
-myNumber = myNumber + 10;
-console.log(myNumber);
+// let myNumber = 8;
+// myNumber = myNumber + 10;
+// console.log(myNumber);
 
-const myNull = null;
-console.log(myNull);
+// const myNull = null;
+// console.log(myNull);
 
-const userName = prompt("Введіть своє ім'я");
-alert(userName);
-console.log(typeof userName, userName);
+// const userName = prompt("Введіть своє ім'я");
+// alert(userName);
+// console.log(typeof userName, userName);
 
-const answer = confirm("Ви дійсно хочете покинути сторінку?");
-console.log(answer);
+// const answer = confirm("Ви дійсно хочете покинути сторінку?");
+// console.log(answer);
 
-alert("Увага! Дія може бути небезпечною.");
-confirm("Ви впевнені, що хочете продовжити?");
+// alert("Увага! Дія може бути небезпечною.");
+// confirm("Ви впевнені, що хочете продовжити?");
