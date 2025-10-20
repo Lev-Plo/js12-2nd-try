3184bd637ef9d6b8884a41f12a7932c35a05108c
Kostiantyn Plotnyk <kostiantyn.plotnyk@gmail.com>
2025-10-20 18:05:01 +0200

    fix for no comma in the end

--- hw8part-1.js
+++ hw8part-1.js
@@ -1,6 +1,10 @@
 const array = ["My", "dog", "is", "cute"];
 let stringNew = "";
 for (let i = 0; i < array.length; i++) {
+    if (i === array.length - 1) {
+        stringNew = stringNew + array[i];
+        break
+    }
     stringNew = stringNew + array[i] + ",";
 }
 console.log(stringNew);
